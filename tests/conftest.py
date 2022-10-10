cd9f2aad9ceb3b388e44654bf8dbf24fb33a1460
Pureevil <elenapureevil@gmail.com>
2022-10-10 19:47:14 +0300

    add allure step

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from  selene.support.shared  import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.hold_browser_open = True
-    browser.config.timeout = 20
-    browser.config.window_width = 1900
-    browser.config.window_height = 1100
+    # browser.config.hold_browser_open = True
+    # browser.config.timeout = 20
+    # browser.config.window_width = 1900
+    # browser.config.window_height = 1100
--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,5 @@
+import allure
+
 from demoqa_tests.model.application_manager import app
 from demoqa_tests.model.date import User
 
@@ -21,31 +23,32 @@ student = User(
 
 
 def test_register_student():
-    #Given
-    app.registration_form.open()
-    #When
-    app.registration_form.set_first_name(student.first_name)
-    app.registration_form.set_last_name(student.last_name)
-    app.registration_form.set_email(student.email)
-    app.registration_form.set_gender(student.gender)
-    app.registration_form.set_mobile_number(student.mobile_number)
-    app.registration_form.set_birth_day(student.birth_year, student.birth_month, student.birth_day)
-    app.registration_form.set_subjects(student.subject)
-    app.registration_form.set_hobbies(student.hobbi)
-    app.registration_form.set_picture(student.photo)
-    app.registration_form.set_address(student.current_address)
-    app.registration_form.set_state(student.state)
-    app.registration_form.set_city(student.city)
-    app.registration_form.submit()
+    with allure.step("Open registrations form"):
+        app.registration_form.open()
 
-    #Then
-    app.resalt.sould_have_row_with_exact_texst(0, 'Student Name', student.full_name(student.first_name, student.last_name))
-    app.resalt.sould_have_row_with_exact_texst(1, 'Student Email', student.email)
-    app.resalt.sould_have_row_with_exact_texst(2, 'Gender', student.gender)
-    app.resalt.sould_have_row_with_exact_texst(3, 'Mobile', student.mobile_number)
-    app.resalt.sould_have_row_with_exact_texst(4, 'Date of Birth', student.birth_day_and_month(student.birth_day, student.birth_month, student.birth_year))
-    app.resalt.sould_have_row_with_exact_texst(5, 'Subjects', student.subjects(student.subject))
-    app.resalt.sould_have_row_with_exact_texst(6, 'Hobbies', student.hobbies(student.hobbi))
-    app.resalt.sould_have_row_with_exact_texst(7, 'Picture', student.photo)
-    app.resalt.sould_have_row_with_exact_texst(8, 'Address', student.current_address)
-    app.resalt.sould_have_row_with_exact_texst(9, 'State and City', student.state_and_city(student.state, student.city))
+    with allure.step("Fill form"):
+        app.registration_form.set_first_name(student.first_name)
+        app.registration_form.set_last_name(student.last_name)
+        app.registration_form.set_email(student.email)
+        app.registration_form.set_gender(student.gender)
+        app.registration_form.set_mobile_number(student.mobile_number)
+        app.registration_form.set_birth_day(student.birth_year, student.birth_month, student.birth_day)
+        app.registration_form.set_subjects(student.subject)
+        app.registration_form.set_hobbies(student.hobbi)
+        app.registration_form.set_picture(student.photo)
+        app.registration_form.set_address(student.current_address)
+        app.registration_form.set_state(student.state)
+        app.registration_form.set_city(student.city)
+        app.registration_form.submit()
+
+    with allure.step("Check form results"):
+        app.resalt.sould_have_row_with_exact_texst(0, 'Student Name', student.full_name(student.first_name, student.last_name))
+        app.resalt.sould_have_row_with_exact_texst(1, 'Student Email', student.email)
+        app.resalt.sould_have_row_with_exact_texst(2, 'Gender', student.gender)
+        app.resalt.sould_have_row_with_exact_texst(3, 'Mobile', student.mobile_number)
+        app.resalt.sould_have_row_with_exact_texst(4, 'Date of Birth', student.birth_day_and_month(student.birth_day, student.birth_month, student.birth_year))
+        app.resalt.sould_have_row_with_exact_texst(5, 'Subjects', student.subjects(student.subject))
+        app.resalt.sould_have_row_with_exact_texst(6, 'Hobbies', student.hobbies(student.hobbi))
+        app.resalt.sould_have_row_with_exact_texst(7, 'Picture', student.photo)
+        app.resalt.sould_have_row_with_exact_texst(8, 'Address', student.current_address)
+        app.resalt.sould_have_row_with_exact_texst(9, 'State and City', student.state_and_city(student.state, student.city))
