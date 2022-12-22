20dc317551b8949d182883d0f08865802e290abb
Pureevil <elenapureevil@gmail.com>
2022-12-22 11:02:07 +0200

    refactoring

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene.support.shared import browser
@@ -12,10 +15,10 @@ def pytest_addoption(parser):
         '--browser_version',
         default='100.0'
     )
-#
-# @pytest.fixture(scope='session', autouse=True)
-# def load_env():
-#     load_dotenv()
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
@@ -32,11 +35,11 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver=driver
